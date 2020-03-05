66c8effb75e7f5b684fb21bba2bced82c7c31e52
Malnati <ricardomalnati@gmail.com>
2020-03-05 00:10:19 -0300

    Adding Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-export default function Square() {
-    return ( <
-        div >
-
-        <
-        /div>
+export default function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
     )
 }
