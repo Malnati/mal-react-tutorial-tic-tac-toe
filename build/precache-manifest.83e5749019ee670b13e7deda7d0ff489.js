1d3a60413d4b1486357f3acf1139cacdf8134fb5
malnati <ricardo.malnati@gmail.com>
2020-10-01 00:21:47 -0300

    It's Aliveeeee

---
+++ build/precache-manifest.83e5749019ee670b13e7deda7d0ff489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2f50fe59d207a14502e9941b760590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e6a9f526c1eeee08b53",
+    "url": "/static/css/main.c0da7f5d.chunk.css"
+  },
+  {
+    "revision": "5ece7a1d25748b4b3958",
+    "url": "/static/js/2.b98eaaa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b98eaaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6a9f526c1eeee08b53",
+    "url": "/static/js/main.8aca856b.chunk.js"
+  },
+  {
+    "revision": "27bed9405e1dacd505d7",
+    "url": "/static/js/runtime-main.2cbc6713.js"
+  }
+]);
